305ce7244a35ef4d752b17f22dc76549b840be0c
B1lol-dev <sardorovichgamer@gmail.com>
2025-04-30 02:32:58 +0500

    added products to home page

---
+++ src/components/Cards/ProductCard.ts
@@ -0,0 +1,41 @@
+// assets
+import star_icon from "../../assets/icons/star_icon.svg";
+import add_to_bag_icon from "../../assets/icons/add_to_bag_icon.svg";
+
+export const ProductCard = (data: any): string => {
+  return /*html*/ `
+    <a href="/product/${data.id}" onclick="location.pathname = '/product/${
+    data.id
+  }'">
+        <div class="flex flex-col max-w-[232px] w-full gap-3">
+            <div class="w-full h-[300px] flex items-center justify-center">
+                <img src=${data.image} alt=${
+    data.title
+  } class="w-full h-full object-contain">
+            </div>
+            <div class="flex flex-col items-start py-3 px-2">
+                <h2 class="text-um-shark text-sm h-10 overflow-y-scroll">${
+                  data.title
+                }</h2>
+                <p class="flex items-center text-xs text-um-manatee mt-2 font-light">
+                    <img src=${star_icon} alt="star" class="mr-1">
+                    ${data.rating.rate}
+                    (${data.rating.count} sharsh)
+                </p>
+                <div class="bg-um-diesel flex justify-center items-center text-xs rounded px-1.5 py-0.5 mt-2">
+                    ${Number(data.price / 8).toFixed(2)} USD/oyiga
+                </div>
+                <div class="flex justify-between items-end w-full mt-5">
+                    <div>
+                        <p class="text-xs line-through text-um-manatee">${Number(
+                          data.price / 0.9
+                        ).toFixed(2)} USD</p>
+                        <h3 class="text-sm text-um-shark">${data.price} USD</h3>
+                    </div>
+                    <button type="button" class="border-1 border-um-manatee p-1 rounded-full"><img src=${add_to_bag_icon} alt="+"></button>
+                </div>
+            </div>
+        </div>
+    </a>
+    `;
+};
--- src/pages/Home/Home.ts
+++ src/pages/Home/Home.ts
@@ -1,3 +1,6 @@
+import axios from "axios";
+import { API_URL } from "../../constants/constants";
+
 // components
 import { Container } from "../../components/Container";
 import { Header } from "../../components/Header";
@@ -5,8 +8,27 @@ import { Footer } from "../../components/Footer";
 
 // assets
 import main_page_banner_img from "../../assets/banners/main_page_banner.png";
+import { ProductCard } from "../../components/Cards/ProductCard";
 
 export const Home = (): string => {
+  setTimeout(() => {
+    const home_products_wrapper = document.getElementById(
+      "home_products_wrapper"
+    )! as HTMLDivElement;
+
+    axios
+      .get(`${API_URL}/products`)
+      .then((res) => {
+        const { data: products } = res;
+
+        const productsHTML = products
+          .map((product: any) => ProductCard(product))
+          .join("");
+        home_products_wrapper.innerHTML = productsHTML;
+      })
+      .catch((err) => console.error(err));
+  }, 0);
+
   return /*html*/ `
     ${Header()}
     <main class="mt-6">
@@ -14,6 +36,13 @@ export const Home = (): string => {
         ${Container(/*html*/ `
           <img src=${main_page_banner_img} alt="main page banner" class="rounded-xl max-w-[1240px] w-full h-[413px]">  
         `)}
+      </section>
+      <section class="pt-12">
+          ${Container(/*html*/ `
+            <div id="home_products_wrapper" class="grid justify-items-center grid-cols-5 gap-x-5 gap-y-8">
+            
+            </div>  
+          `)}
       </section>
     </main>
     ${Footer()}
