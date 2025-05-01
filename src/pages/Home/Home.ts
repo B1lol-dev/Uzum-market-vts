900a7f662e213a907494404bb903db3d235a89dc
B1lol-dev <sardorovichgamer@gmail.com>
2025-05-01 19:15:13 +0500

    added swiper to home page

--- src/pages/Home/Home.ts
+++ src/pages/Home/Home.ts
@@ -1,14 +1,21 @@
 import axios from "axios";
 import { API_URL } from "../../constants/constants";
+import { register } from "swiper/element/bundle";
 
 // components
 import { Container } from "../../components/Container";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
+import { ProductCard } from "../../components/Cards/ProductCard";
 
 // assets
 import main_page_banner_img from "../../assets/banners/main_page_banner.png";
-import { ProductCard } from "../../components/Cards/ProductCard";
+import main_page_banner_img_2 from "../../assets/banners/main_page_banner_2.jpg";
+import main_page_banner_img_3 from "../../assets/banners/main_page_banner_3.jpg";
+import main_page_banner_img_4 from "../../assets/banners/main_page_banner_4.jpg";
+
+// register swiper
+register();
 
 export const Home = (): string => {
   setTimeout(() => {
@@ -29,12 +36,29 @@ export const Home = (): string => {
       .catch((err) => console.error(err));
   }, 0);
 
+  const bannerImages = [
+    main_page_banner_img,
+    main_page_banner_img_2,
+    main_page_banner_img_3,
+    main_page_banner_img_4,
+  ];
+
   return /*html*/ `
     ${Header()}
     <main>
       <section>
         ${Container(/*html*/ `
-          <img src=${main_page_banner_img} alt="main page banner" class="rounded-xl max-w-[1240px] w-full h-[413px]">  
+          <swiper-container navigation="true" pagination="true" class="rounded-xl max-w-[1240px] w-full h-[413px] overflow-hidden">
+            ${bannerImages
+              .map(
+                (img: string) => /*html*/ `
+                  <swiper-slide lazy="true" class="w-full">
+                    <img src=${img} loading="lazy" class="w-full" />
+                  </swiper-slide>
+                `
+              )
+              .join("")}
+          </swiper-container>
         `)}
       </section>
       <section class="pt-12">
