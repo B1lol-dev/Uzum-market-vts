9ffe2179469ebbc9be2e6aa445a257f1c231d984
B1lol-dev <sardorovichgamer@gmail.com>
2025-05-02 01:59:43 +0500

    added products counter to cart page

--- src/pages/Cart/Cart.ts
+++ src/pages/Cart/Cart.ts
@@ -14,15 +14,25 @@ export const Cart = () => {
       "cart_products_wrapper"
     )! as HTMLDivElement;
 
+    const cart_title_count = document.getElementById(
+      "cart_title_count"
+    )! as HTMLSpanElement;
+
     axios
       .get(`${API_URL}/carts/1`)
       .then((res) => {
         const products = res.data.products;
 
+        cart_title_count.innerText = products.length + " mahsulot";
+
         const productsHTML = products
           .map((product: any) => CartProductCard(product))
           .join("");
         cart_products_wrapper.innerHTML = productsHTML;
+
+        setInterval(() => {
+          cart_title_count.innerText = `${cart_products_wrapper.children.length} mahsulot`;
+        }, 1000);
       })
       .catch((err) => console.error(err));
   }, 0);
@@ -34,7 +44,7 @@ export const Cart = () => {
             ${Container(/*html*/ `
                 <div class="flex">
                     <div>
-                      <h1 id="cart_title" class="text-2xl font-bold text-um-shark">Savatingiz, <span class="text-um-manatee">0 mahsulot</span></h1>
+                      <h1 class="text-2xl font-bold text-um-shark">Savatingiz, <span id="cart_title_count" class="text-um-manatee">0 mahsulot</span></h1>
                       <div id="cart_products_wrapper" class="flex flex-col p-4 gap-5 border-1 border-um-manatee rounded-[2px] mt-5">
                           ${Array(10)
                             .fill("")
